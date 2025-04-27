eb3c863bfaf1fe03549295e059b2b50bd2f84df3
Route-Reflector <tonkorera113@gmail.com>
2025-04-27 22:32:51 +0900

    🚀 feat: pingコマンドとインタラクティブモード実装。 🐸

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import subprocess, argparse
 import cmd2
 
+import ping
+
+
+"""
+cmd3のコマンドライン引数はすべて文字列型となるため、注意が必要。
+たとえば、Trueと入力しても実際には"True"となるため型変換が必要。
+"""
 
 class KeroRoute(cmd2.Cmd):
-    prompt = "🐸\033[92mKeroRoute> \033[0m"
+    # prompt = "🐸\033[92mKeroRoute> \033[0m"
+    prompt = "🐸\033[38;5;190mKeroRoute> \033[0m"
+
 
     def initial_message(self):
         with open("kero-data/kero-logo.txt", "r") as logo_data:
@@ -12,6 +22,11 @@ class KeroRoute(cmd2.Cmd):
     def do_exit(self, _):
         self.poutput("\nKeroRouteを終了するケロ🐸🔚\n")
         return True 
+          
+
+
+
+KeroRoute.do_ping = ping.do_ping
 
 
 if __name__ == "__main__":
---
+++ ping.py
@@ -0,0 +1,91 @@
+import subprocess, argparse, ipaddress
+import cmd2
+
+
+######################
+### PARSER_SECTION ###
+######################
+ping_parser = argparse.ArgumentParser()
+ping_parser.add_argument("ip", nargs="?", default=None)
+ping_parser.add_argument("-c", "--count", type=int, default=4)
+ping_parser.add_argument("-s", "--size", type=int, default=56)
+ping_parser.add_argument("-t", "--ttl", type=int, default=64)
+ping_parser.add_argument("-l", "--log", action="store_true")
+
+
+@cmd2.with_argparser(ping_parser)
+def do_ping(self, args):
+    if not args.ip:
+        return _do_ping_interactive(self)
+
+    try:
+        ipaddress.IPv4Address(args.ip)
+    except ipaddress.AddressValueError:
+        self.poutput("IPアドレス間違ってないケロ？🐸")
+        return
+    
+    if args.log:
+        #　ここにログを保存する処理。
+        self.poutput("💾ログ保存モードONケロ🐸🔛")
+
+    try:
+        subprocess.run(["ping", args.ip, "-c", str(args.count), "-s", str(args.size), "-t", str(args.ttl)], check=True)
+    except subprocess.CalledProcessError:
+        self.poutput("🚨なんか失敗したケロロ.....🐸")
+        return
+
+    self.poutput("Pingの結果を確認するケロ🐸")
+
+
+def _do_ping_interactive(self) -> None:
+
+    ip: str = input("どのIP/ホストにpingするケロ？🐸")        
+    if ip == "":
+        self.poutput("IPアドレスは必須ケロ！🐸")
+        return
+    else:
+        try:
+            ipaddress.IPv4Address(ip)
+        except ipaddress.AddressValueError:
+            self.poutput("IPアドレス間違ってないケロ？🐸")
+            return
+
+
+    # repeatの最大値を決めておく？
+    repeat: int = input("何回送信するケロ？(default = 4)🐸: ")        
+    if repeat == "":
+        repeat = 4
+    else:
+        repeat = int(repeat)
+    
+
+    # pacekt_sizeの最小値と最大値も決めて置く？
+    packet_size: int = input("packetsizeはどうするケロ？(default = 56)🐸: ")
+    if packet_size == "":
+        packet_size = 56
+    else:
+        packet_size = int(packet_size)        
+
+
+    # ttl は0から255の間かな？
+    ttl = input("ttlはどうするケロ？🐸(default = 64): ")        
+    if ttl == "":
+        ttl = 64
+    else:
+        ttl = int(ttl)
+
+
+    save_log: bool = False    
+    log: str = input("ログは保存するケロ？🐸(yes/no): ")        
+    if log == "":
+        log = "no"
+
+    if log.lower() == "yes":
+        save_log = True
+        self.poutput("💾ログ保存モードONケロ🐸🔛")
+        # ここにログを保存する処理。
+
+
+    self.poutput("ping実行中.....🐸💨")                  
+    subprocess.run(["ping", ip, "-c", str(repeat), "-s", str(packet_size), "-t", str(ttl)], check=True)
+    self.poutput("実行終了ケロ。実行結果を確認するケロ🐸🔚")        
